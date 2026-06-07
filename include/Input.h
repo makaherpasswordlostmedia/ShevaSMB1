3f3ee81d45effcdf7c58c14960fe456097174000
whyineedname <emssaoig1@gmail.com>
2026-06-07 13:14:55 +0300

    Add files via upload

--- include/Input.h
+++ include/Input.h
@@ -3,6 +3,7 @@
 #include <SDL2/SDL.h>
 
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 enum class Key : int
@@ -18,7 +19,7 @@ enum class Key : int
    MENU_DOWN,
    MENU_LEFT,
    MENU_RIGHT,
-   MENU_ACCEPT,  // Select the current option
+   MENU_ACCEPT,
    MENU_ESCAPE,
    PAUSE,
 };
@@ -33,7 +34,16 @@ class Input {
 
    void set(const Key action, SDL_Scancode keyCode);
 
+   // Called each frame with keyboard state
    void update(const Uint8* keystates);
+
+#ifdef ANDROID
+   // Called from Java via JNI when a virtual button is pressed/released
+   void setVirtualKey(Key key, bool pressed);
+
+   // Called from Java to inject a virtual key press for menu navigation
+   void pressVirtualKey(Key key);
+#endif
 
    SDL_Scancode getBoundKey(Key action);
 
@@ -54,9 +64,16 @@ class Input {
 
    Input(const Input&) = delete;
 
-   std::vector<SDL_Scancode> currentRawKeys;  // All keys on the keyboard
+   std::vector<SDL_Scancode> currentRawKeys;
 
    std::unordered_map<Key, SDL_Scancode> keyBindings;
    std::unordered_map<Key, bool> keysPressed;
    std::unordered_map<Key, bool> keysHeld;
+
+#ifdef ANDROID
+   // Virtual keys set by touch overlay (independent of keyboard)
+   std::unordered_map<Key, bool> virtualKeys;
+   // Keys that were virtually pressed this frame (one-shot)
+   std::unordered_set<Key> virtualPressedThisFrame;
+#endif
 };
